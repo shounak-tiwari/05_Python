0256917c2d79a6a29c5858efc6ea7c13f188444f
Akash Tiwari <154492266+shounak-tiwari@users.noreply.github.com>
2025-04-10 22:27:02 +0530

    Add files via upload

---
+++ Apr-10/03_lit.py
@@ -0,0 +1,12 @@
+#floating point litrals
+
+float_num = 20.6
+scientific_num = 2.9e-3  #2900
+negative_flotiing = -20.77
+
+print(float_num)
+print(scientific_num)
+print(negative_flotiing)
+
+#2 9 0 0 0 0 0 0 0 0 .0
+
